9dffb24d24ab13d22ed95a59d1db773cf56b54ad
kapatbl4 <vvi93@yandex.ru>
2022-01-12 23:48:42 +0300

    Progression fix

--- src/main/java/hexlet/code/games/Progression.java
+++ src/main/java/hexlet/code/games/Progression.java
@@ -47,14 +47,14 @@ public class Progression {
             hiddenNumberPosition--;
         }
         StringBuilder sb = new StringBuilder();
-
+        if (hiddenNumberPosition == 0) {
+            sb.append("..");
+        } else {
+            sb.append(numbers[0]);
+        }
         for (int i = 1; i < numbers.length; i++) {
             if (i == hiddenNumberPosition) {
-                if (sb.length() == 0) {
-                    sb.append("..");
-                } else {
-                    sb.append(" ..");
-                }
+                sb.append(" ..");
                 continue;
             }
             sb.append(" ").append(numbers[i]);
